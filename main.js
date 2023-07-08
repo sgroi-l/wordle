cb4c6def09c994d85203e205ccfe6b299c5a05e9
sgroil <laurencesgroi@hotmail.co.uk>
2023-07-08 11:38:26 +0100

    added mobile functionality

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ import { handleKeyPress } from "./utils/keyboard.js";
 
 document.addEventListener('keyup', handleKeyPress);
 
+
+const mobileInput = document.querySelector('#mobileInput');
+mobileInput.addEventListener('input', handleMobileInput);
+
+function handleMobileInput(event) {
+  const key = event.target.value.slice(-1);
+
+  if (/^[a-zA-Z]$/.test(key)) {
+    fillTile(key);
+  } else if (key === 'Backspace') {
+    deleteLastTile();
+  } else if (key === 'Enter' && filledTileCount === 5) {
+    evaluateAnswer();
+  }
+
+
+  event.target.value = '';
+}
+
