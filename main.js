59cebf1b57deb63aa8b387300e436d6cbb073238
sgroil <laurencesgroi@hotmail.co.uk>
2023-07-08 12:01:47 +0100

    removed mobile functionality

--- main.js
+++ main.js
@@ -2,25 +2,3 @@ import { handleKeyPress } from "./utils/keyboard.js";
 
 document.addEventListener('keyup', handleKeyPress);
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const mobileInput = document.querySelector('#mobileInput');
-    mobileInput.addEventListener('input', handleMobileInput);
-    mobileInput.focus();
-  });
-
-function handleMobileInput(event) {
-  const key = event.target.value.slice(-1);
-
-  if (/^[a-zA-Z]$/.test(key)) {
-    fillTile(key);
-  } else if (key === 'Backspace') {
-    deleteLastTile();
-  } else if (key === 'Enter' && filledTileCount === 5) {
-    evaluateAnswer();
-  }
-
-
-  event.target.value = '';
-}
-
